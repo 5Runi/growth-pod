4700ed3b7fc3539ef699a35105b914387f3e0a37
5Runi <rgeldner@pm.me>
2025-10-17 15:05:44 -0400

    completed

--- debugging.js
+++ debugging.js
@@ -1,10 +1,10 @@
 // Example 1:
 
-function greet(name {
+function greet(name) {
   return "Hello, " + name;
 }
 
-// console.log(greet("Erica"));
+console.log("Example 1: ", greet("Erica"));
 
 // Expected result:
 // Should print "Hello, Erica"
@@ -18,10 +18,10 @@ function calculateAvg(prices) {
   for (const price of prices) {
     total += price; // total = total + price; // 10, 30, 60
   }
-  return total / prices.length - 1;
+  return total / prices.length;
 }
 
-console.log(calculateAvg([10, 20, 30]));
+console.log("Example 2: ", calculateAvg([10, 20, 30]));
 
 // Expected result: 20
 
@@ -32,7 +32,7 @@ console.log(calculateAvg([10, 20, 30]));
 function countVowels(str) {
   const vowels = ["a", "e", "i", "o", "u"];
   let count = 0;
-  for (let i = 0; i <= str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     if (vowels.includes(str[i].toLowerCase())) {
       count++;
     }
@@ -40,7 +40,7 @@ function countVowels(str) {
   return count;
 }
 
-console.log(countVowels("Hello"));
+console.log("Example 3: ", countVowels("Hello"));
 
 // Expected result:
 // Actual result:
@@ -50,15 +50,24 @@ console.log(countVowels("Hello"));
 
 function findIndex(arr, target) {
   let i = 0;
+  const found =[];
   while (i < arr.length) {
     if (arr[i] === target) {
-      return i;
+      found.push(i);
     }
+    i++; //needed to increment counter; should have known this- maybe can compare
+    //a while loop and a for loop on flash cards- cuz really need all of the for loop 
+    //components, right?
   }
-  return -1;
+  return found;
 }
 
-console.log(findIndex([2, 4, 6, 8], 6));
+const testThis = [2, 5, 9, 2, 10]
+console.log("can you see me?")
+
+//findIndex();
+console.log(findIndex(testThis, 2));
+console.log("Example 4: ", findIndex([2, 4, 6, 8], 6));
 
 // Expected result: 2
 
@@ -69,6 +78,7 @@ function doubleValues(numbers) {
   return numbers.map(num => num * 2);
 }
 
-console.log(doubleValues(5));
+console.log("Example 5: ", doubleValues([5]));//map is an array method, needed to 
+//put value into an array
 
 // Expected result:
